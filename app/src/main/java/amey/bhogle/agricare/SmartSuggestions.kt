3d88d34334eadfc11f6a0058fbcde3a52de8e4bf
Manoranjan-Baral <manoranjan.baral@testlify.com>
2025-11-29 02:06:05 +0530

    fix route

--- app/src/main/java/amey/bhogle/agricare/SmartSuggestions.kt
+++ app/src/main/java/amey/bhogle/agricare/SmartSuggestions.kt
@@ -76,7 +76,7 @@ fun SmartSuggestions(navController: NavController){
                     context.getString(R.string.nav_home) -> navController.navigate("home")
                     context.getString(R.string.nav_suggest) -> navController.navigate("suggestion")
                     context.getString(R.string.nav_language) -> navController.navigate("language")
-                    context.getString(R.string.nav_help) -> navController.navigate("help")
+                    context.getString(R.string.nav_help) -> navController.navigate("health_tips") // Updated route
                     context.getString(R.string.nav_settings) -> navController.navigate("settings")
                 }
             }
